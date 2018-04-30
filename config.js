0ccc584b39ef406a7ea26aa3cac355e7f8f7ab53
Gabriel Muñumel <gabrielmunumel@gmail.com>
2018-04-30 17:06:35 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+var appsettings = {
+    consumerkey: 'qSQkHibQRv2KUMC9BIroPiIZw',
+    consumersecret: 'aIcRp69NfoRqXKdVtwjnsbIOLCgyx8dlhGIqqNvMJnzTZnaI57',
+    bearertoken: ''
+};
+
+module.exports = appsettings;
