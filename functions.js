659737638ec288e88bdad4f2d45e1be04b3eacf8
Gabriel Munumel <gabrielmunumel@gmail.com>
2018-05-02 12:22:52 +0200

    Fixed parameter to get more than 15 tweets

--- functions.js
+++ functions.js
@@ -10,7 +10,7 @@ functions = {
         request.post('https://api.twitter.com/oauth2/token', {form: {'grant_type': 'client_credentials'},
         headers: {Authorization: finalheader}}, function(error, response, body) {
             if(error)
-            console.log(error);
+                console.log(error);
             else {
                 config.bearertoken = JSON.parse(body).access_token;
 
@@ -23,10 +23,11 @@ functions = {
         var searchquery = req.body.query;
         var encsearchquery = encodeURIComponent(searchquery);
         var bearerheader = 'Bearer ' + config.bearertoken;
+
         request.get('https://api.twitter.com/1.1/search/tweets.json?q=' + encsearchquery +
-         '&result_type=recent', {headers: {Authorization: bearerheader}}, function(error, body, response) {
+         '&result_type=recent&count=100', {headers: {Authorization: bearerheader}}, function(error, body, response) {
              if(error)
-             console.log(error);
+                console.log(error);
              else {
                  res.json({success: true, data:JSON.parse(body.body)});
              }
